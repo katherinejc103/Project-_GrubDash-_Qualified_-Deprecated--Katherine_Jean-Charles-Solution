7c274d778a3a2c16de68cf5d3cefa8e0d02e5ba9
Katherine <katherinejc103@gmail.com>
2024-04-03 09:50:41 -0400

    pushing to see

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -8,14 +8,11 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+let lastDishId = dishes.reduce((maxId, dish) => Math.max(maxId, dish.id), 0);
 
 function create(req, res) {
-  
-  let lastDishId = dishes.reduce((maxId, dish) => Math.max(maxId, dish.id), 0);
-
 //   app.post("/dishes", (req, res, next) => {
 //     const {data: { name, description, price, image_url } = {} } = req.body;
-  
     const newDish = {
         id: ++lastDishId,
         name: name,
@@ -28,6 +25,45 @@ function create(req, res) {
  
 }
 
+function bodyDataHas(propertyName) {
+    return function (req, res, next) {
+      const { data = {} } = req.body;
+      if (data[propertyName]) {
+        return next();
+      }
+      next({
+          status: 400,
+          message: `Must include a ${propertyName}`
+      });
+    };
+  }
+
+  function descriptionPropertyValid(req, res, next){
+    const { data: { description } = {} } = req.body;
+    const validDescription = [""]
+    if (validDescription.includes(description)){
+        return next()
+    }
+    next({
+        status: 400,
+        message: `Value of the 'description' property must be one of ${validDescription}. Received: ${description}`,
+      });
+
+
+
+    function imagePropertyValid(req, res, next){
+        const { data: { image_url } = {} } = req.body;
+        const validImage = [""]
+        if (validImage.includes(image_url)){
+            return next()
+        }
+        next({
+            status: 400,
+            message: `Value of the 'image_url' property must be one of ${validImage}. Received: ${image_url}`,
+          });
+    
+      }
+
 function list(req, res){
  const {dishId} = req.params
  res.json({ data: dishes.filter(dishId ? dish => dish.id == dishId : () => true) });
@@ -35,6 +71,10 @@ function list(req, res){
 }
 
 module.exports = {
+    create: [     
+        bodyDataHas("name"),
+        descriptionPropertyValid,
+        imageValid],
     create,
     list
 }
