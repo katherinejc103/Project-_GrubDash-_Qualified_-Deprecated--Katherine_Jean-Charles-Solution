b03d91e52038da25515fccae0cec1cc036fb116a
Katherine <katherinejc103@gmail.com>
2024-04-09 10:37:53 -0400

    adding updated dishes controller

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -1,80 +1,117 @@
-
 const path = require("path");
-
-// Use the existing dishes data
 const dishes = require(path.resolve("src/data/dishes-data"));
-
-// Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /dishes handlers needed to make the tests pass
-let lastDishId = dishes.reduce((maxId, dish) => Math.max(maxId, dish.id), 0);
-
-function create(req, res) {
-//   app.post("/dishes", (req, res, next) => {
-//     const {data: { name, description, price, image_url } = {} } = req.body;
-    const newDish = {
-        id: ++lastDishId,
-        name: name,
-        description: description,
-        price: price,
-        image_url : image_url,
-    }
-    dishes.push(newDish);
-    res.status(201).json({ data: newDish });
- 
-}
+//Middleware
 
 function bodyDataHas(propertyName) {
-    return function (req, res, next) {
-      const { data = {} } = req.body;
-      if (data[propertyName]) {
-        return next();
-      }
-      next({
-          status: 400,
-          message: `Must include a ${propertyName}`
-      });
-    };
-  }
-
-  function descriptionPropertyValid(req, res, next){
-    const { data: { description } = {} } = req.body;
-    const validDescription = [""]
-    if (validDescription.includes(description)){
-        return next()
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+    if (data[propertyName]) {
+      return next();
     }
-    next({
-        status: 400,
-        message: `Value of the 'description' property must be one of ${validDescription}. Received: ${description}`,
-      });
-
-
-
-    function imagePropertyValid(req, res, next){
-        const { data: { image_url } = {} } = req.body;
-        const validImage = [""]
-        if (validImage.includes(image_url)){
-            return next()
-        }
-        next({
-            status: 400,
-            message: `Value of the 'image_url' property must be one of ${validImage}. Received: ${image_url}`,
-          });
-    
-      }
-
-function list(req, res){
- const {dishId} = req.params
- res.json({ data: dishes.filter(dishId ? dish => dish.id == dishId : () => true) });
-
+    next({ status: 400, message: `Must include a ${propertyName}` });
+  };
 }
+
+function priceIsValidNumber(req, res, next) {
+  const { data: { price } = {} } = req.body;
+  if (price <= 0 || !Number.isInteger(price)) {
+    return next({
+      status: 400,
+      message: `price`,
+    });
+  }
+  next();
+}
+
+function dishExists(req, res, next) {
+  const { dishId } = req.params;
+  const foundDish = dishes.find((dish) => dish.id === dishId);
+  if (foundDish) {
+    res.locals.dish = foundDish;
+    next();
+  } else {
+    next({ status: 404, message: `Dish does not exist: ${dishId}.` });
+  }
+}
+
+function bodyIdMatchesRouteId(req, res, next) {
+  const { data: { id } = {} } = req.body;
+  const dishId = res.locals.dish.id;
+  if (id) {
+    const matchesRouteId = id === dishId;
+    return matchesRouteId
+      ? next()
+      : next({
+          status: 400,
+          message: `Dish id does not match route id. Dish: ${id}, Route: ${dishId}`,
+        });
+  } else next();
+}
+
+//
+//Route handlers
+
+function create(req, res) {
+  const { data: { name, description, price, image_url } = {} } = req.body;
+  const newId = nextId();
+  const newDish = {
+    id: newId,
+    name: name,
+    description: description,
+    price: Number(price),
+    image_url: image_url,
+  };
+  dishes.push(newDish);
+  res.status(201).json({ data: newDish });
+}
+
+function read(req, res) {
+  res.json({ data: res.locals.dish });
+}
+
+function update(req, res) {
+  const { data: { name, description, price, image_url } = {} } = req.body;
+  const dishId = res.locals.dish.id;
+  const updatedDish = {
+    id: dishId,
+    name: name,
+    description: description,
+    price: Number(price),
+    image_url: image_url,
+  };
+  const index = dishes.findIndex((dish) => dish.id === dishId);
+  dishes.splice(index, 1, updatedDish);
+  res.json({ data: updatedDish });
+}
+
+function list(req, res) {
+  res.json({ data: dishes });
+}
+
+//
+//Exports
 
 module.exports = {
-    create: [     
-        bodyDataHas("name"),
-        descriptionPropertyValid,
-        imageValid],
+  create: [
+    bodyDataHas("name"),
+    bodyDataHas("description"),
+    bodyDataHas("price"),
+    bodyDataHas("image_url"),
+    priceIsValidNumber,
     create,
-    list
-}
+  ],
+  read: [dishExists, read],
+  update: [
+    dishExists,
+    bodyIdMatchesRouteId,
+    bodyDataHas("name"),
+    bodyDataHas("description"),
+    bodyDataHas("price"),
+    bodyDataHas("image_url"),
+    priceIsValidNumber,
+    update,
+  ],
+  list,
+};
